f08ff3a3c5b4daf93dccbc430adba548ffa4a383
Naseef Ameer <naasfameer@gmail.com>
2025-08-23 22:52:38 +0530

    fix: route list corrected

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,14 @@ Route::get('/', function () {
     return response()->json(['message' => 'Route not found'], 404);
 });
 
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-});
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('articles', ArticleController::class);
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('articles', ArticleController::class);
+    });
 });
