e5349086df004a61f96b1bd93603e1b65896dda4
Naseef Ameer <naasfameer@gmail.com>
2025-09-15 22:42:34 +0530

    User Register notification

---
+++ app/Notifications/WelcomeUserNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeUserNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Welcome to Our Platform')
+            ->greeting('Hello ' . $notifiable->name . ',')
+            ->line('We’re excited to have you on board!')
+            ->action('Visit Dashboard', url('/'))
+            ->line('Thank you for registering!');
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'message' => 'Welcome ' . $notifiable->name . '! Thanks for joining us.',
+            'url' => url('/'),
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
