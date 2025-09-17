edb10864e7ab0bd8bc7820c8927279beb599442d
Naseef Ameer <naasfameer@gmail.com>
2025-09-17 21:51:03 +0530

    feat:common Rest API functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ApiService;
+
 abstract class Controller
 {
-    //
+    protected ApiService $api;
+
+    public function __construct(ApiService $api)
+    {
+        $this->api = $api;
+    }
 }
