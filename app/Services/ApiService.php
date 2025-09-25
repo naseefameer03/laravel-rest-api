967f512a5a778bf9a0f7b85e77e655348e1f3928
Naseef Ameer <naasfameer@gmail.com>
2025-09-25 22:28:16 +0530

    fix: code quality fix

--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class ApiService
