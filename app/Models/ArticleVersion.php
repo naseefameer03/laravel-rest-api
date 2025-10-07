db756ab4724e323d00ecca51a576c795260a05e0
Naseef Ameer <naasfameer@gmail.com>
2025-10-07 20:17:37 +0530

    feat: ArticleVersion model relationships added

--- app/Models/ArticleVersion.php
+++ app/Models/ArticleVersion.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArticleVersion extends Model
 {
-    //
+    protected $fillable = [
+        'article_id',
+        'version',
+        'created_by',
+        'title',
+        'excerpt',
+        'content',
+        'status'
+    ];
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
