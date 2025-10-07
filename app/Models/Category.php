cea6d56d3fcf32a05848333ef3ebcf867cad6b27
Naseef Ameer <naasfameer@gmail.com>
2025-10-07 20:11:12 +0530

    feat: Category model relationships added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['parent_id', 'name', 'slug', 'description'];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class);
+    }
 }
