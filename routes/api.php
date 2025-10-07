68ff3230ea7ae1ee949e5a3b4805859211a6f4a6
Naseef Ameer <naasfameer@gmail.com>
2025-10-07 22:55:30 +0530

    feat: Updated api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\ArticleVersionController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
@@ -20,12 +22,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy'])
         ->middleware('permission:delete users');
 
-    // Route::prefix('v1')->group(function () {
-    Route::apiResource('articles', ArticleController::class);
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('articles', ArticleController::class);
+        Route::post('articles/{article}/publish', [ArticleController::class, 'publish']);
+        Route::post('articles/{article}/images', [ArticleController::class, 'uploadImages']);
+        Route::post('articles/{article}/cover', [ArticleController::class, 'replaceCover']);
 
-    Route::post('/messages/send', [MessageController::class, 'send']);
-    Route::get('/messages/conversation/{userId}', [MessageController::class, 'conversation']);
-    Route::get('/messages/inbox', [MessageController::class, 'inbox']);
-    Route::patch('/messages/{id}/read', [MessageController::class, 'markAsRead']);
-    // });
+        Route::get('articles/{article}/versions', [ArticleVersionController::class, 'index']);
+        Route::get('articles/{article}/versions/{version}', [ArticleVersionController::class, 'show']);
+        Route::post('articles/{article}/versions/{version}/revert', [ArticleVersionController::class, 'revert']);
+
+        Route::get('articles/{article}/comments', [CommentController::class, 'index']);
+        Route::post('articles/{article}/comments', [CommentController::class, 'store']);
+        Route::put('comments/{comment}', [CommentController::class, 'update']);
+        Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
+
+        Route::post('/messages/send', [MessageController::class, 'send']);
+        Route::get('/messages/conversation/{userId}', [MessageController::class, 'conversation']);
+        Route::get('/messages/inbox', [MessageController::class, 'inbox']);
+        Route::patch('/messages/{id}/read', [MessageController::class, 'markAsRead']);
+    });
 });
