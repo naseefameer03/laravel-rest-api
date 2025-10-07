a4faaa969766c7e4e7d072ee554f9c46cc904625
Naseef Ameer <naasfameer@gmail.com>
2025-10-07 22:57:45 +0530

    feat: added ArticleVersion and Comment controller

---
+++ app/Http/Controllers/Api/ArticleVersionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\ArticleVersion;
+
+class ArticleVersionController extends Controller
+{
+    public function index(Article $article)
+    {
+        return $article->versions()->orderByDesc('version')->paginate(20);
+    }
+
+    public function show(Article $article, ArticleVersion $version)
+    {
+        abort_unless($version->article_id === $article->id, 404);
+        return $version;
+    }
+
+    public function revert(Article $article, ArticleVersion $version)
+    {
+        abort_unless($version->article_id === $article->id, 404);
+
+        $article->update([
+            'title'   => $version->title,
+            'excerpt' => $version->excerpt,
+            'content' => $version->content,
+            'status'  => $version->status,
+        ]);
+
+        return $article->fresh();
+    }
+}
